6a618a82da669ee7f4d114f24610add99f26922a
Alex Antipin <alex@labs42.com>
2011-09-27 12:05:24 +0400

    Updated jquery version, some cleanup in init.js and page.css

--- demo/js/init.js
+++ demo/js/init.js
@@ -1,33 +1,35 @@
-$(document).ready(function(){
-  
-  var myConsole = $('#console');
-  
-  $('#handySelect-1').handySelect({
-    selectedOptionsToDisplay: 'all',
-    onChange: function(value){
-      console.log(value)
-    }
+(function ($) {
+
+  $(document).ready(function(){
+    
+    $('#handySelect-1').handySelect({
+      selectedOptionsToDisplay: 'all',
+      onChange: function(value){
+        console.log(value)
+      }
+    });
+    
+    $('#handySelect-2').handySelect({
+      selectedOptionsToDisplay: 2,
+      selectorMode: 'count',
+      onChange: function(value){
+        console.log(value)
+      }
+    });
+    
+    $('#handySelect-3').handySelect({
+      selectedOptionsToDisplay: 3,
+      onChange: function(value){
+        console.log(value)
+      }
+    });
+    
+    $('#handySelect-inline').handySelect({
+      onChange: function(value){
+        console.log(value)
+      }
+    })
+    
   });
-  
-  $('#handySelect-2').handySelect({
-    selectedOptionsToDisplay: 2,
-    selectorMode: 'count',
-    onChange: function(value){
-      console.log(value)
-    }
-  });
-  
-  $('#handySelect-3').handySelect({
-    selectedOptionsToDisplay: 3,
-    onChange: function(value){
-      console.log(value)
-    }
-  });
-  
-  $('#handySelect-inline').handySelect({
-    onChange: function(value){
-      console.log(value)
-    }
-  })
-  
-});
+
+}(jQuery));
